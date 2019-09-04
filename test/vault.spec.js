f4553aedf11a167b2643926055c3956dadf49dd3
Dong Wook Kim <dehypnosis@gmail.com>
2019-09-04 12:03:44 +0900

    coverage 100%

---
+++ test/vault.spec.js
@@ -0,0 +1,174 @@
+const vault = require("../src");
+
+const opts = {
+    uri: "https://vault.internal.qmit.pro",
+    method: "k8s/dev",
+    role: "default",
+    debug: true,
+};
+
+
+describe("test vault.async function", () => {
+    it("should throw for invalid args", () => {
+        expect(vault.async()).rejects.toThrow();
+        expect(vault.async("here should be factory")).rejects.toThrow();
+        expect(vault.async(() => {}, "here should be object")).rejects.toThrow();
+    });
+
+    it("should be failed to create configuration for invalid item (with local token)", () => {
+        expect(
+            vault.async(async read => {
+                return {
+                    a: (await read("common/data/test-invalid-path")).data.hello,
+                    b: 2,
+                    c: 3,
+                }
+            }, {
+                ...opts,
+            })
+        )
+            .rejects
+            .toThrow();
+    });
+
+    it("should create configuration asynchronously (with local token)", () => {
+        return expect(
+            vault.async(async read => {
+                return {
+                    a: (await read("common/data/test")).data.hello,
+                    b: 2,
+                    c: 3,
+                }
+            }, {
+                ...opts,
+            })
+        )
+            .resolves
+            .toMatchObject({
+                a: "world",
+                b: 2,
+                c: 3,
+            });
+    });
+
+    it("should create configuration asynchronously (with k8s sa token)", () => {
+        return expect(
+            vault.async(async read => {
+                return {
+                    a: (await read("common/data/test")).data.hello,
+                    b: (await read("common/data/test")).data.hello,
+                    c: 3,
+                }
+            }, {
+                ...opts,
+                ignoreLocalToken: true,
+            })
+        )
+            .resolves
+            .toMatchObject({
+                a: "world",
+                b: "world",
+                c: 3,
+            });
+    });
+
+
+    it("should be failed to create configuration without any token", () => {
+        expect(
+            vault.async(async read => {
+                return {
+                    a: (await read("common/data/test")).data.hello,
+                    b: 2,
+                    c: 3,
+                }
+            }, {
+                ...opts,
+                ignoreLocalToken: true,
+                ignoreK8sSAToken: true,
+            })
+        )
+            .rejects
+            .toThrow();
+    });
+});
+
+
+describe("test vault function", () => {
+    it("should throw for invalid args", () => {
+        expect(() => vault("here should be factory")).toThrow();
+        expect(() => vault(() => {}, "here should be object")).toThrow();
+    });
+
+    it("should create configuration synchronously (with local token)", () => {
+        return expect(
+            vault(async read => {
+                return {
+                    a: (await read("common/data/test")).data.hello,
+                    b: 2,
+                    c: 3,
+                }
+            }, {
+                ...opts,
+            })
+        )
+            .toMatchObject({
+                a: "world",
+                b: 2,
+                c: 3,
+            });
+    });
+
+    it("should create configuration synchronously (with k8s sa token)", () => {
+        return expect(
+            vault(async read => {
+                return {
+                    a: (await read("common/data/test")).data.hello,
+                    b: (await read("common/data/test")).data.hello,
+                    c: 3,
+                }
+            }, {
+                ...opts,
+                ignoreLocalToken: true,
+            })
+        )
+            .toMatchObject({
+                a: "world",
+                b: "world",
+                c: 3,
+            });
+    });
+
+
+    it("should be failed to create configuration for invalid item (with local token)", () => {
+        expect(() => {
+            vault(async read => {
+                return {
+                    a: (await read("common/data/test-invalid-path")).data.hello,
+                    b: 2,
+                    c: 3,
+                }
+            }, {
+                ...opts,
+            });
+        })
+            .toThrow();
+    });
+
+    it("should be failed to create configuration without any token", () => {
+        expect(() => {
+            vault(async read => {
+                return {
+                    a: (await read("common/data/test")).data.hello,
+                    b: 2,
+                    c: 3,
+                }
+            }, {
+                ...opts,
+                ignoreLocalToken: true,
+                ignoreK8sSAToken: true,
+            });
+        })
+            .toThrow();
+    });
+});
+
